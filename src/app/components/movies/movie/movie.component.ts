671b26cb504be7fd97dc773b9d3ce94551a81945
vishwasracharya <vishwasracharya@gmail.com>
2022-10-18 15:53:22 +0530

    get single movie

--- src/app/components/movies/movie/movie.component.ts
+++ src/app/components/movies/movie/movie.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 import { MovieService } from './movie.service';
+
+const API_BASE_URL = environment.apiUrl;
 
 @Component({
   selector: 'app-movie',
@@ -8,13 +12,20 @@ import { MovieService } from './movie.service';
   styleUrls: ['./movie.component.css'],
 })
 export class MovieComponent implements OnInit {
-  constructor(private route: ActivatedRoute, private service: MovieService) {}
+  movie: any;
+  isMovieAvailable = false;
+
+  constructor(private route: ActivatedRoute, private httpClient: HttpClient) {}
 
   ngOnInit(): void {
     const id = this.route.snapshot.paramMap.get('id');
-    console.log(id);
-    this.service.getMovie().subscribe((data) => {
-      console.log(data);
+    const url = `${API_BASE_URL}api/movie/${id}`;
+    const headers = {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${environment.authToken}`,
+    };
+    this.httpClient.get(url, { headers }).subscribe((data) => {
+      this.movie = data;
     });
   }
 }
